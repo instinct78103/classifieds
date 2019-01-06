803af390315488538e4c601f6c06e34817dc1db2
Stas <media_newspaper@mail.ru>
2019-01-07 01:05:13 +0600

    Теперь поиск по регулярному выражению происходит и в FILEENEW, найденные строки будут выделены красным

--- !!!test.php
+++ !!!test.php
@@ -2,5 +2,6 @@
 require_once('variables.php');
 /* echo count(array_count_values(file(FILEOLD))) . '<br>';
 foreach(array_count_values(file(FILEOLD)) as $key=>$item){
-	file_put_contents("new17.txt", "$key\n", FILE_APPEND);
-} */
+	file_put_contents('created ' . date("Y-m-d") . '.txt', "$key\n", FILE_APPEND);
+}
+ */
--- show_old.php
+++ show_old.php
@@ -1,13 +1,30 @@
 ﻿<?php
 require_once('variables.php');
 
+//Поиск среди свежих объявлений, они будут выделены красным
+$fileArrNew = file_exists(FILENEW) ? file(FILENEW) : [];
+$fileArrNew = array_values(array_filter($fileArrNew, "trim"));
+
+foreach($fileArrNew as $key=>$str){
+	$fileArrNew[$key] = trim(iconv("windows-1251", "utf-8", $str));
+	if( $_POST['find'] && preg_match("~{$_POST['find']}~ui", $str) ){
+		echo '<p class="matches already">' . $str . '</p>';
+	}
+}
+
+
+//Поиск среди старых объявлений
 $fileArr = file(FILEOLD);
 $fileArr = array_values(array_filter($fileArr, "trim"));
 
 foreach($fileArr as $key=>$str){
 	$fileArr[$key] = trim(iconv("windows-1251", "utf-8", $str));
-	if( $_POST['find'] &&  preg_match("~{$_POST['find']}~ui", $str) ){
+	if( $_POST['find'] && preg_match("~{$_POST['find']}~ui", $str) ){
 		echo '<p class="matches">' . $str . '</p>';
 	}
 }
+
+
+
+
 ?>
